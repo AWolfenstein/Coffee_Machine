6199980f86d97ffd7d60e7dffd9bd68ba1f2e65e
AWolfenstein <alexsupa@mail.ru>
2020-12-26 18:30:05 +0300

    start_front

--- Coffee_Machine/Coffee_Machine.cpp
+++ Coffee_Machine/Coffee_Machine.cpp
@@ -2,10 +2,97 @@
 //
 
 #include <iostream>
+#include "swti/swti.hpp"
+using namespace std;
+void headLogo();
+void balance(int byn);
+bool mainMenu();
+
+void printVerticalLine(int x, int y) {
+
+	Cursor.printChar(x, y + 1, DLINE_V);
+
+};
+
+void printFrame(int x, int y, int columns, int rows)
+{
+	// corners
+	Cursor.printChar(x, y, DLINE_UR);
+	Cursor.printChar(x, y + rows + 1, DLINE_DR);
+	Cursor.printChar(x + columns + 1, y, DLINE_UL);
+	Cursor.printChar(x + columns + 1, y + rows + 1, DLINE_DL);
+
+	// horizontal bars
+	for (int i = x + 1; i <= x + columns; i++)
+	{
+		Cursor.printChar(i, y, DLINE_H);
+		Cursor.printChar(i, y + rows + 1, DLINE_H);
+	}
+
+	// vertical bars
+	for (int i = y + 1; i <= y + rows; i++)
+	{
+		Cursor.printChar(x, i, DLINE_V);
+		Cursor.printChar(x + columns + 1, i, DLINE_V);
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Cursor.setFontPixels(15, 20);
+	Window.setSizeChars(30, 25);
+	Window.hideResize();
+	Window.hideBlinking();
+	Window.hideScrollbars();
+	Window.setTitle(L"EspressoBiancci LEI700");
+
+	Cursor.setColor(YELLOW);
+	printFrame(2, 0, 25, 23);
+
+	headLogo();
+	while (mainMenu()) {}
+	return 0;
+}
+
+void headLogo() {
+	Cursor.setColor(WHITE);
+	printFrame(4, 1, 21, 1);
+	Cursor.setPosition(8, 2);
+	Cursor.setColor(LIGHTYELLOW);
+	cout << "Espresso";
+	Cursor.setPosition(16, 2);
+	Cursor.setColor(LIGHTGREEN);
+	cout << "Biancci";
+}
+
+void balance(int byn) {
+	Cursor.setColor(GREEN);
+	printFrame(4, 4, 21, 1);
+	Cursor.setPosition(6, 5);
+	Cursor.setColor(WHITE);
+	cout << "Balance: ";
+	Cursor.setPosition(18, 5);
+	Cursor.setColor(LIGHTCYAN);
+	cout << byn;//BYN
+	Cursor.setPosition(22, 5);
+	Cursor.setColor(LIGHTYELLOW);
+	cout << "BYN";
+}
+
+
+bool mainMenu() {
+	int rows = Window.getRows();
+	balance(999);
+
+	Cursor.setColor(YELLOW);
+	printFrame(3, 7, 23, 15);
+
+	Cursor.setColor(WHITE);
+	printFrame(4, 8, 21, 1);
+	printVerticalLine(7, 8);
+	printVerticalLine(17, 8);
+	Keyboard.waitUser();
+	return false;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
